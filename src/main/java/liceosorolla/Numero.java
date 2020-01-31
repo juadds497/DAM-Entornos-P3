009931a56c47ca17274f203ab06b7a0ca7fcdeae
DAM_1 <DAM_1@PB03>
2020-01-31 20:49:07 +0100

    Mejora 4

--- src/main/java/liceosorolla/Numero.java
+++ src/main/java/liceosorolla/Numero.java
@@ -1,6 +1,30 @@
 package main.java.liceosorolla;
 
 public class Numero {
-	int numero = 4;
+	
+	public static void primo(int numero){
+		int contador = 0;
+		
+		for(int i = 1; i <= numero; i++)
+        {
+            if((numero % i) == 0)
+            {
+                contador++;
+            }
+        }
+ 
+        if(contador <= 2)
+        {
+            System.out.println("El numero "+numero+ " es primo");
+        }else{
+            System.out.println("El numero "+numero+ " no es primo");
+        }
+	}
+
+	public static void main(String[] args) {
+		int numero = 4;
+		primo(numero);
+
+	}
 	
 }
