ef0bf7dcd1fb93305abe29f6b8e18f8ee533dba6
DAM_1 <DAM_1@PB04>
2020-01-31 20:01:05 +0100

    Mejora 1 (maximo)

--- src/main/java/liceosorolla/Arrays.java
+++ src/main/java/liceosorolla/Arrays.java
@@ -1,9 +1,31 @@
 package main.java.liceosorolla;
 
+
+
 public class Arrays {
 
 	public static void main(String[] args) {
+		
 		int[] array = {1,2,3,4,5};
-	}
 
-}
+		 
+		 System.out.println("El máximo es " + maximo(array));
+		
+		
+	}//MAIN
+
+
+
+	public static int maximo(int[] num){
+		
+		int max;
+		max=num[0];
+		for(int i = 0; i < num.length; i++){
+			if(max<num[i]){
+				max=num[i];
+			}
+		}
+	
+		return max;
+	}
+}//CLASS
