eb6e67dfbe8489200e39fa44a3beb56ee7db3adb
DAM_1 <DAM_1@PB03>
2020-01-31 20:19:51 +0100

    Mejora2

--- src/main/java/liceosorolla/Arrays.java
+++ src/main/java/liceosorolla/Arrays.java
@@ -1,9 +1,24 @@
 package main.java.liceosorolla;
 
 public class Arrays {
+	
+	public static void minimo (int[]array) {
+		int min = 999;
+		 
+		for(int i = 0; i < array.length; i++)
+		{
+			if(min>array[i])
+			{
+				min=array[i];
+			}
+		}
+ 
+		System.out.println("El valor minimo es: "+min);
+	}
 
 	public static void main(String[] args) {
 		int[] array = {1,2,3,4,5};
+		
+		minimo(array);
 	}
-
 }
--- src/main/java/liceosorolla/Numero.java
+++ src/main/java/liceosorolla/Numero.java
@@ -1,6 +1,22 @@
 package main.java.liceosorolla;
 
 public class Numero {
-	int numero = 4;
+	
+	public static void factorial(int numero){
+		int resultado = 1;
+		
+		for(int i = 1; i<= numero;i++) {
+			 resultado = resultado * i;
+		 }
+		System.out.println("El factorial es: "+resultado);
+		
+	}
+	
+	public static void main(String[] args) {
+		int numero = 4;
+		
+		factorial(numero);
+	}
+	
 	
 }
